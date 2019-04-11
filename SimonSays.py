c333a74ae7102a68c431ce031d94908b9a3a5b45
Michael Jones <mikey.wj@live.co.uk>
2019-04-11 12:52:31 +0100

    Started simon says work. Cycles all leds

---
+++ SimonSays.py
@@ -0,0 +1,26 @@
+
+from microbit import *
+
+#red_led = 0
+#orange_led = 1
+#yellow_led = 8
+#green_led = 12
+
+while True:
+    pin0.write_digital(1)
+    sleep(500)
+    pin0.write_digital(0)
+    sleep(500)
+    pin1.write_digital(1)
+    sleep(500)
+    pin1.write_digital(0)
+    sleep(500)
+    pin8.write_digital(1)
+    sleep(500)
+    pin8.write_digital(0)
+    sleep(500)
+    pin12.write_digital(1)
+    sleep(500)
+    pin12.write_digital(0)
+    sleep(500)
+    
